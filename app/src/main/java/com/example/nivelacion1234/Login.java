a5faf5b00c94066179d51a1c1e9c57cc22d6e602
alpaezt <113650189+alpaezt@users.noreply.github.com>
2022-12-22 12:03:36 -0500

    Spring 4

--- app/src/main/java/com/example/nivelacion1234/Login.java
+++ app/src/main/java/com/example/nivelacion1234/Login.java
@@ -1,5 +1,6 @@
 package com.example.nivelacion1234;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     private Button btnLogin, btnLoginRegister;
@@ -25,7 +31,24 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = editEmailLogin.getText().toString();
+                String pass = editPassLogin.getText().toString();
 
+                FirebaseAuth mAuth;
+                // ...
+                // Initialize Firebase Auth
+                mAuth = FirebaseAuth.getInstance();
+
+                mAuth.signInWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(getApplicationContext(), Catalogo.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
             }
         });
 
